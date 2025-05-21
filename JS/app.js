4f0694ede968aa1af32eaa1cc11689bd36efe0e7
vickytoria14 <vickytoria0514@gmail.com>
2025-05-21 12:40:05 +0900

    0521-up

---
+++ JS/app.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded",function(){
+
+    const progressBar = document.querySelectorAll(".progress-bar");
+
+    progressBar.forEach(bar =>{
+        const target = bar.getAttribute('data-bs-target');
+        setTimeout(() =>{
+            bar.style.width = target + '%';
+
+        },500)
+
+
+    })
+
+
+
+
+
+
+})
